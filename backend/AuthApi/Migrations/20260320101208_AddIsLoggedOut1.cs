a322275a03f1f4354dbfb1d61dc7ff83b8363ebc
Nvhuma <nhlayisekobvuma7@gmail.com>
2026-03-20 12:19:49 +0200

    docs: add Postman logout endpoint screenshot

---
+++ backend/AuthApi/Migrations/20260320101208_AddIsLoggedOut1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AuthApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsLoggedOut1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLoggedOut",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsLoggedOut",
+                table: "Users");
+        }
+    }
+}
--- backend/AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
+++ backend/AuthApi/obj/Debug/net9.0/AuthApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2df0c33679800699a5190d2660b5a8f7ea18b8d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b43905f4bc53e6792bed3cb0ae0f7420361ff79")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
